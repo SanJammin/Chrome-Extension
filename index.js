072f7354c9735feea6bafad499cc15cfca33acc1
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-22 18:13:42 +0000

    Take value from input element and push into myLeads array

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function() {
-
+    myLeads.push(inputEl.value);
 });
