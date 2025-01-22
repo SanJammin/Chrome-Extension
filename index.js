9bbd1499785c44d6a9432481fd4ff182076ef689
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-22 18:19:16 +0000

    Create ul element to display leads and grab element in js

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let myLeads = [];
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
 });
+
+for (let i = 0; i < myLeads.length; i++){
+
+};
