3ce011eb41565a9569d6f64b31cff23e4130b9a2
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-24 20:09:06 +0000

    Broke lines up for better readability

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++){
         listItems += `
             <li>
-                <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+                <a href='${myLeads[i]}' target='_blank'>
+                ${myLeads[i]}
+                </a>
             </li>
         `;
     };
