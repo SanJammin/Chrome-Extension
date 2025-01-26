b3ab3122b6b5084819fd02b10c7ffd1c3bfddfaf
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-26 21:46:58 +0000

    Update firebase database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 
 const firebaseConfig = {
-
+    databaseURL : "https://leads-tracker-app-58db7-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 let myLeads = [];
 
