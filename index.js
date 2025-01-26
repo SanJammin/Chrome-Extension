478ccdad5c7daeb033740e8074728b4f2fba3dd5
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-26 16:07:34 +0000

    Fetch tab from url (TEST)

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
-}
+};
+
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen"}
+];
+
+tabBtn.addEventListener("click", function() {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads);
+});
 
 function render(leads) {
     let listItems = "";
@@ -26,7 +38,7 @@ function render(leads) {
     };
     
     ulEl.innerHTML = listItems;
-}
+};
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
