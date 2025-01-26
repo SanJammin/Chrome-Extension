3574ee6060175db20506ffcfcc500d4e2dca19a1
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-26 21:43:07 +0000

    Link to firebase

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+
+const firebaseConfig = {
+
+};
+
+const app = initializeApp(firebaseConfig);
+
 let myLeads = [];
 
 const inputEl = document.getElementById("input-el");
