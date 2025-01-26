afc499ea4086ee931295be778d3898f40841832f
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-26 12:22:26 +0000

    Allow leads to stay on page after refresh

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
